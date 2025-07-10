aa5091cb6f57bbb5c62fc519217d85e08e8f1dac
Hildebrando <jose.hildebrando@gmail.com>
2025-07-09 23:37:38 -0300

    Estrutura básica do home

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css'
 import './styles/global.css'
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
 
     return (
     <>
-      <Heading>
-      Olá mundo 1 
-      <button>
-        <TimerIcon/>
-      </button>
-      </Heading>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Obcaecati laboriosam numquam natus, ipsa nam architecto rerum tempora non 
-        laborum hic minima possimus, maiores libero, labore quae nesciunt perferendis 
-        recusandae sapiente?
-        </p>
+      <div className="container">
+        <div className="content">
+          <section>
+            Icon
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>
+            Chronos
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>
+            Form
+          </section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>
+            Footer
+          </section>
+        </div>
+      </div>
     </>)
 }
